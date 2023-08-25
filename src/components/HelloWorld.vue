6d4b9a59618956bb254f34623f30a359c9d51dda
subin oh <gaeulhaneul10@plateer.com>
2023-08-25 13:22:09 +0900

    file init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,124 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="q-pa-md">
+    <div class="q-gutter-md row items-start">
+      <q-card flat bordered style="max-width: 250px">
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-separator inset />
+
+        <q-card-section>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </q-card-section>
+      </q-card>
+
+      <q-list class="shadow-2 rounded-borders" style="max-width: 250px; width: 100%;">
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar6.jpg">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Jane</q-item-section>
+        </q-item>
+
+        <q-separator inset="item" />
+
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar3.jpg">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Lily</q-item-section>
+        </q-item>
+
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Mary</q-item-section>
+        </q-item>
+      </q-list>
+
+      <q-list class="shadow-2 rounded-borders" style="max-width: 250px; width: 100%;">
+        <q-item>
+          <q-item-section thumbnail>
+            <img src="https://cdn.quasar.dev/img/mountains.jpg">
+          </q-item-section>
+          <q-item-section>Mountains</q-item-section>
+        </q-item>
+
+        <q-separator inset="item-thumbnail" />
+
+        <q-item>
+          <q-item-section thumbnail>
+            <img src="https://cdn.quasar.dev/img/parallax1.jpg">
+          </q-item-section>
+          <q-item-section>Venice</q-item-section>
+        </q-item>
+
+        <q-item>
+          <q-item-section thumbnail>
+            <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+          </q-item-section>
+          <q-item-section>The Bridge</q-item-section>
+        </q-item>
+      </q-list>
+
+      <q-list class="bg-grey-9 text-white shadow-2 rounded-borders" style="max-width: 250px; width: 100%;">
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar6.jpg">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Jane</q-item-section>
+        </q-item>
+
+        <q-separator inset dark />
+
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar3.jpg">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Lily</q-item-section>
+        </q-item>
+
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Mary</q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </div>
 </template>
+
+<style>
+</style>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  created() {
+    this.getPosts();
+  },
+  methods: {
+    getPosts() {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+          .then((response) => response.json())
+          .then((json) => console.log(json));
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
